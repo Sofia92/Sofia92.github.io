c8f6745a89ba91d2614973239894fa9ba6f87c3b
于秀芳 <yu.xiufang@synyi.com>
2025-03-13 21:26:05 +0800

    move docs

--- _bin/extractMarkdowns.js
+++ _bin/extractMarkdowns.js
@@ -40,8 +40,8 @@ function deep(dir) {
         } else {
             const nameKey = Symbol();
             const isMarkdown = item.endsWith('.md'), isPdf = item.endsWith('.pdf');
-            const relativePath = path.relative(path.join(__dirname, '../'), itemPath);
-            const filePath = `https://github.com/Sofia92/Sofia92.github.io/raw/refs/heads/master/${relativePath}`;
+            const relativePath = path.relative(path.join(__dirname, '../docs'), itemPath);
+            const filePath = `./${relativePath}`;
             if (isMarkdown) {
                 const title = item.split('.md')[0];
                 const response = fs.readFileSync(itemPath, 'utf-8');
@@ -54,7 +54,7 @@ function deep(dir) {
             if (isPdf) {
                 const title = item.split('.pdf')[0];
                 markdownMap.set(nameKey, {
-                    title, category: null, ...attributes,
+                    title, category: null,
                     isMarkdown, isPdf, path: filePath
                 });
             }
@@ -63,6 +63,6 @@ function deep(dir) {
 }
 
 let markdownMap = new Map(), pdfMap = new Map();
-deep(path.join(__dirname, '../_posts'));
+deep(path.join(__dirname, '../docs'));
 
 fs.writeFileSync('docs/list.json', JSON.stringify([...markdownMap.values()].concat([...pdfMap.values()])));
